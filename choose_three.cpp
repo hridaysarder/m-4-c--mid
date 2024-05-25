5b215a5ab0adc26aee0b177e5d9df4944c510fa6
HridaySarder <hridoyaug12@gmail.com>
2024-05-25 11:36:20 +0600

    added choose three

---
+++ choose_three.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, s;
+        cin >> n >> s;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        int found = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                for (int k = j + 1; k < n; k++)
+                {
+                    if (a[i] + a[j] + a[k] == s)
+                    {
+                        found = 1;
+                        break;
+                    }
+                }
+            }
+        }
+        if (found)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
